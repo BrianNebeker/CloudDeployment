a77b377c6eb2632e19c9a71a11980fa51c26083d
Brian <brian@mrmicropc.com>
2022-08-16 19:38:31 -0600

    Add test of application IP

--- azure/kubeapply-demo-azure.sh
+++ azure/kubeapply-demo-azure.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 # Connect docker to Amazon Repo
 az acr login --name briannebeker
+az aks get-credentials --resource-group dev_group --name dev
 
 kubectl apply -f ./deploy/azure/demo-azure.yaml
+kubectl get ingress ingress-app-demo -o jsonpath='{.status.loadBalancer.ingress[0].ip}' > ingressip
+url="http:\\$(<ingressip)"
+printf "\nOutput from URL $url \n"
+curl $url
+printf "n\n"

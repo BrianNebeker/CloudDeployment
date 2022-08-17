a3fe0bfa489aaf3200f2de3af1c2efbd5a08d7b4
Brian <brian@mrmicropc.com>
2022-08-16 19:39:38 -0600

    Fix url

--- azure/kubeapply-demo-azure.sh
+++ azure/kubeapply-demo-azure.sh
@@ -5,7 +5,7 @@ az aks get-credentials --resource-group dev_group --name dev
 
 kubectl apply -f ./deploy/azure/demo-azure.yaml
 kubectl get ingress ingress-app-demo -o jsonpath='{.status.loadBalancer.ingress[0].ip}' > ingressip
-url="http:\\$(<ingressip)"
+url="http://$(<ingressip)"
 printf "\nOutput from URL $url \n"
 curl $url
 printf "n\n"

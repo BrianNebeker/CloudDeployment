10e0e4261e10eb0150860bc5df84a4769202b5c7
Brian <brian@mrmicropc.com>
2022-08-17 16:17:56 -0600

    Tweak deployment waiter

--- azure/kubeapply-demo-azure.sh
+++ azure/kubeapply-demo-azure.sh
@@ -9,8 +9,8 @@ kubectl apply -f ./deploy/azure/demo-azure.yaml
 printf "\nWaiting for rollout of application\n"
 
 ATTEMPTS=0
-ROLLOUT_STATUS_CMD="kubectl rollout status deployment/app-demo"
-until $ROLLOUT_STATUS_CMD || [ $ATTEMPTS -eq 60 ]; do
+ROLLOUT_STATUS_CMD="kubectl rollout status deployment/app-demo -w --timeout=10s"
+until $ROLLOUT_STATUS_CMD || [ $ATTEMPTS -eq 30 ]; do
   $ROLLOUT_STATUS_CMD
   ATTEMPTS=$((attempts + 1))
   sleep 10
@@ -18,8 +18,10 @@ done
 
 kubectl get all
 
+# Get the ingress public IP and hit the application to verify the deployment
 kubectl get ingress ingress-app-demo -o jsonpath='{.status.loadBalancer.ingress[0].ip}' > ingressip
 url="http://$(<ingressip)"
+
 printf "\nOutput from URL $url \n"
 curl $url
 printf "\n\n"

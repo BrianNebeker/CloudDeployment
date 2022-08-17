ad3b1cbfbdd4e8063fd7f27cf837e737dae89bf0
Brian <brian@mrmicropc.com>
2022-08-16 20:59:49 -0600

    Add wait for rollout

--- azure/kubeapply-demo-azure.sh
+++ azure/kubeapply-demo-azure.sh
@@ -4,6 +4,16 @@ az acr login --name briannebeker
 az aks get-credentials --resource-group dev_group --name dev
 
 kubectl apply -f ./deploy/azure/demo-azure.yaml
+
+# Check deployment rollout status every 10 seconds (max 10 minutes) until complete.
+ATTEMPTS=0
+ROLLOUT_STATUS_CMD="kubectl rollout status deployment/myapp -n namespace"
+until $ROLLOUT_STATUS_CMD || [ $ATTEMPTS -eq 60 ]; do
+  $ROLLOUT_STATUS_CMD
+  ATTEMPTS=$((attempts + 1))
+  sleep 10
+done
+
 kubectl get all
 
 kubectl get ingress ingress-app-demo -o jsonpath='{.status.loadBalancer.ingress[0].ip}' > ingressip

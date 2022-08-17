4993226e6cf9c5c17a03ae4caf45678f47b4946c
Brian <brian@mrmicropc.com>
2022-08-16 20:42:06 -0600

    Add more kubernetes logging

--- azure/kubeapply-demo-azure.sh
+++ azure/kubeapply-demo-azure.sh
@@ -4,6 +4,8 @@ az acr login --name briannebeker
 az aks get-credentials --resource-group dev_group --name dev
 
 kubectl apply -f ./deploy/azure/demo-azure.yaml
+kubectl get all
+
 kubectl get ingress ingress-app-demo -o jsonpath='{.status.loadBalancer.ingress[0].ip}' > ingressip
 url="http://$(<ingressip)"
 printf "\nOutput from URL $url \n"

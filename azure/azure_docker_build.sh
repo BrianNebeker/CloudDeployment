695a3c62d7087be633b50a4abdc57d2a17b02896
Brian <brian@mrmicropc.com>
2022-08-17 22:07:52 -0600

    Add update build in script

--- azure/azure_docker_build.sh
+++ azure/azure_docker_build.sh
@@ -6,8 +6,9 @@ az acr login --name briannebeker
 mkdir -p azbuild
 cp ./Docker/* ./azbuild/
 cp ./Node/* ./azbuild/
+sed -i "s/%BUILD%/${1}/g" ./azbuild/script.js
 docker build -t demo-azure ./azbuild
 
 # Push to Azure repo
-docker tag demo-azure:latest briannebeker.azurecr.io/demo-azure
+docker tag demo-azure:$1 briannebeker.azurecr.io/demo-azure:latest
 docker push briannebeker.azurecr.io/demo-azure:latest

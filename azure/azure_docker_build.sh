78f0fc269294fe7458608e058e3245f8e00943f6
Brian <brian@mrmicropc.com>
2022-08-17 22:16:40 -0600

    Remove redundant docker build

--- azure/azure_docker_build.sh
+++ azure/azure_docker_build.sh
@@ -2,13 +2,6 @@
 # Connect docker to Amazon Repo
 az acr login --name briannebeker
 
-# Build docker image
-mkdir -p azbuild
-cp ./Docker/* ./azbuild/
-cp ./Node/* ./azbuild/
-sed -i "s/%BUILD%/${1}/g" ./azbuild/script.js
-docker build -t demo-azure ./azbuild
-
 # Push to Azure repo
 docker tag demo-azure:$1 briannebeker.azurecr.io/demo-azure:latest
 docker push briannebeker.azurecr.io/demo-azure:latest
